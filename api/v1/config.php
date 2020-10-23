976bb8dd6886ac0aa282480e723b8be91f12be95
CberYellowstone <2844829687@qq.com>
2020-10-24 01:27:45 +0800

    -

--- api/v1/config.php
+++ api/v1/config.php
@@ -1,17 +1,7 @@
 <?php
-global $video_root_path;
-global $data_path;
-global $version;
+include_once '../../config.php';
 $video_root_path=dirname(dirname(dirname(__FILE__))).'/video';
 $data_path=dirname(dirname(dirname(__FILE__))).'/data';
 
-//用户设置
-$site_name = "Yellowstone's Anime Site";
-$version = "Alpha 0.1.0";
-
-//其他设置
-$DanmakuArea = "83%";
-$DanmakuDurationCss = "danmaku 9s linear";
-$About_link = "https://github.com/CberYellowstone/DanDanPlay-PHP";
 
 ?>
