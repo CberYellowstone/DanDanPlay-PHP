ff9158a8317b3193efe00c97c9b49a6728d585d1
CberYellowstone <2844829687@qq.com>
2020-10-23 19:49:14 +0800

    减少除非config

--- config.php
+++ config.php
@@ -2,15 +2,22 @@
 global $video_root_path;
 global $data_path;
 global $version;
+global $web_users;
+global $able_cache;
 $video_root_path=dirname(__FILE__).'/video';
 $data_path=dirname(__FILE__).'/data';
 $version = "Alpha 0.2.0";
 
 //用户设置
 $site_name = "Yellowstone's Anime Site";//站点名称
+$able_cache = TRUE;
 $remote_port = "8009";//远程访问端口号,需为http协议,留空取访问时端口号
 $remote_addres = "apps.ystone.top";//留空则为访问时地址
-$user_name = "Yellowstone";//随便填
+$authorization = FALSE;
+$root_username = "Yellowstone";//随便填
+$web_users=array('Yellowstone' => '12345678',);
+$api_authkey="";
+
 
 //其他设置
 $DanmakuArea = "83%";
