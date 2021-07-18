ac800932dd88f03a1659c584b1e24eed2b428518
CberYellowstone <2844829687@qq.com>
2021-07-19 00:51:50 +0800

    [change] 更改冗杂的ifelse为switch语句

--- function.php
+++ function.php
@@ -130,9 +130,7 @@ function mkpic($video_file,$video_time,$pic_name,$pic_size,$isFouce=FALSE) {
     if(!isExists(dirname($pic_name,1))){
         mkdir(iconv("UTF-8", "GBK", (dirname($pic_name,1))),0777,true); 
     }       
-    //echo($mkpic_command."</br>");
     if(!isExists($pic_name or $isFouce)){
-        //echo ($mkpic_command."</br>");
         system($mkpic_command);
     }
 }
@@ -140,15 +138,12 @@ function mkpic($video_file,$video_time,$pic_name,$pic_size,$isFouce=FALSE) {
 //$mkpic_folder带路径
 function mkpicForFolder($mkpic_folder,$isFouce=FALSE){
     $save_path=$GLOBALS['data_path'];
-    //echo ($save_path."</br>");
     $folder_name = md5(getFileName($mkpic_folder,TRUE));
     foreach((countFolder($mkpic_folder))[1] as $mkpic_video){
         $video_name = md5(getFileName($mkpic_video));
-        //echo ($save_path."/".$folder_name."/".$video_name."</br>");
         if(!isExists($save_path."/".$folder_name."/".$video_name)){
             mkdir(iconv("UTF-8", "GBK", ($save_path."/".$folder_name."/".$video_name)),0777,true); 
         }       
-        //$video_name = formatSpace($video_name);
         if($GLOBALS['able_webp']){
             mkpic($mkpic_video,289,($save_path."/".$folder_name."/".$video_name."/".$video_name.".webp"),'400*225',$isFouce);
         } else{
@@ -240,8 +235,6 @@ function getVideoInformation($file_path){
     preg_match_all('/\"episodeTitle\":(.*?),\"/', $post_result, $episodeTitle_matches);
     $episodeTitle_list = $episodeTitle_matches[1];
     $episodeTitle_first = $episodeTitle_list[0];
-    //echo ($post_result);
-    //print_r(array($episodeId_first,$animeId_first,$animeTitle_first,$episodeTitle_first));
     $i = 0;
     foreach($animeTitle_list as $animeTitle_each){
         if($animeTitle_each == '"'.$animeTitleFromDirName.'"'){
@@ -274,18 +267,11 @@ function saveVideoInformation($file_path,$Force_make=FALSE){
         $video_information_list = getVideoInformation($file_path)[1];
         $video_information_list_named = array('episodeId' => $video_information_list[0], 'animeId' => $video_information_list[1], 'animeTitle' => $video_information_list[2], 'episodeTitle' => $video_information_list[3], 'file_path' => $file_path);
         $video_information_json = json_encode($video_information_list_named,JSON_UNESCAPED_UNICODE);
-        //echo ($video_information_json);
-        //echo ($GLOBALS['data_path'].'/'.$folder_name.'/'.$file_name.'/'.$file_name.'.json');
         if(!isExists($GLOBALS['data_path'].'/'.$folder_name.'/'.$file_name)){
             mkdir(iconv("UTF-8", "GBK", ($GLOBALS['data_path'].'/'.$folder_name.'/'.$file_name)),0777,true); 
         }   
-        //echo($video_information_json.'</br>');    
         file_put_contents($GLOBALS['data_path'].'/'.$folder_name.'/'.$file_name.'/'.$file_name.'.json', $video_information_json);    
-    }//elseif(isExists($GLOBALS['data_path'].'/'.$folder_name.'/'.$file_name.'/'.$file_name.'.json')){
-        //echo (readVideoInformation($file_path)[1]).'</br>';
-        //print_r(readVideoInformation($file_path)[0]['file_path']);
-        //echo ("</br>");
-    //} 
+    }
 }
 
 function readVideoInformation($file_path,$auto_get=FALSE){
@@ -314,18 +300,15 @@ function mkCardForFolder($folder_path,$isSreaching=''){
         $video_file_size = getFileSize($each_video_path);
         $video_time = getVideoTime($each_video_path)[0];
         $video_information_list = readVideoInformation($each_video_path,TRUE)[0];
-        //print_r(getVideoInformation($each_video_path)[1].'</br>');
         $animeTitle = removeQuote($video_information_list['animeTitle']);
         $episodeTitle = removeQuote($video_information_list['episodeTitle']);
         if($isSreaching && !(strstr((HanziConvert::convert($video_file_name.$animeTitle.$episodeTitle)),$isSreaching))){
-            //echo $video_file_name.$animeTitle.$episodeTitle.'</br>';
             continue;
         }
         $video_path = $video_information_list['file_path'];
         $video_parent_path_md5 = md5(getFileName(dirname($video_path),TRUE));
         $video_file_md5 = md5(getFileName($video_path));
         $last_time = readLastTime($each_video_path);
-        //echo ($video_file_md5."</br>");
         echo ('<div class="col-sm-6 col-md-4 float-left pt-4"><div class="card"><a href="./video.php?video='.$video_parent_path_md5."-".$video_file_md5.'"><img class="card-img-top" src="'.$video_pic_link.'" alt="Card image cap"></a><div class="card-body"><h5 class="card-title line-limit-length"><a href="./index.php?animeName='.$animeTitle.'">'.$animeTitle.'</a></h5><h5 class="card-title" style="overflow: hidden; white-space: nowrap;text-overflow: ellipsis"></h5><p class="video-text line-limit-length"><a href="./video.php?video='.$video_parent_path_md5."-".$video_file_md5.'">'.$episodeTitle.'</a><br>'.$video_file_name.'<br>时长：'.$video_time.'<br>文件体积：'.$video_file_size.'<br>上次播放：'.$last_time.'</p></div></div></div>');
     }
 }
@@ -425,13 +408,11 @@ function getCommentFromMD5($md5){
     $comment_text = str_replace(array("\r\n", "\r", "\n", "	"), "", $comment_text);
     header('Content-Type:application/json; charset=utf-8');
     echo $comment_text = is_utf8($comment_text) ? $comment_text : filterUtf8($comment_text);
-    // echo($comment_text);
 }
 
 function mkList($folder_path,$now_path=""){
     foreach(countFolder($folder_path)[1] as $each_path){
         $video_name_all = getFileName($each_path,TRUE);
-        //echo($video_name_all);
         $episodeTitle = removeQuote(readVideoInformation($each_path)[0]['episodeTitle']);
         $video_parent_path_md5 = md5(getFileName(dirname($each_path,1),TRUE));
         $video_file_md5 = md5(getFileName($each_path));
@@ -441,7 +422,6 @@ function mkList($folder_path,$now_path=""){
             echo('active');
         }
         echo('">'.$episodeTitle.'<small>'.$video_name_all.'</small></a>');
-        //echo('</br>');
     }
 }
 
@@ -545,33 +525,15 @@ function checkUserAndPasswordFromCookie($cookieWebUserName,$cookieAuth){
 
 
 
-if($_GET['action']=='listRoot'){
-    //listRoot($video_root_path);
+switch($_GET['action']){
+    case "getCommentFromMD5":
+        getCommentFromMD5($_GET['md5']);
+        break;
+    case "clCache":
+        @header("Location: ./");
+        break;
+    default:
+        sendStatusCode(403,"Forbidden");
 }
-elseif($_GET['action']=='mkpic'){
-    //mkpicForFolder(listRoot($video_root_path,FALSE)[0]);
-}
-elseif($_GET['action']=='getFileName'){ 
-    //echo(getFileName($path_fot_test,TRUE));
-}
-elseif($_GET['action']=='getVideoPic'){
-    //echo (getVideoPic($path_fot_test));
-}
-elseif($_GET['action']=='saveVideoInformation'){
-    //saveVideoInformationForFolder(listRoot($video_root_path,FALSE)[0],FALSE);
-}
-elseif($_GET['action']=='getCommentFromMD5'){
-    getCommentFromMD5($_GET['md5']);
-}
-elseif($_GET['action']=='clCache'){
-    @header("Location: ./");
-}
-elseif($_GET['action']=='test'){
-    echo ("这是一个测试接口!</br>");
-    //saveLastTime('3443dab0dcee781a18e927eb92a50740-e14822833a7c329477d6867001f241c2');
-    echo ("</br>输出结束!</br>");
-}
-
-
 
 ?>
